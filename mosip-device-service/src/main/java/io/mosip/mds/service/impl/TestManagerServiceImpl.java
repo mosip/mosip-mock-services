d0d97459a1f613c961861bff6e7a7f9f4bc0e6b6
Ramesh Narayanan <nrameshmcd@gmail.com>
2020-05-17 17:22:26 +0530

    Fixed start error, changed runs to get

--- mosip-device-service/src/main/java/io/mosip/mds/service/impl/TestManagerServiceImpl.java
+++ mosip-device-service/src/main/java/io/mosip/mds/service/impl/TestManagerServiceImpl.java
@@ -9,16 +9,12 @@ import io.mosip.mds.dto.getresponse.MasterDataResponseDto;
 import io.mosip.mds.dto.getresponse.TestExtnDto;
 import io.mosip.mds.dto.postresponse.RunExtnDto;
 import io.mosip.mds.entitiy.TestManager;
-import io.mosip.mds.repository.TestManagerRepository;
 import io.mosip.mds.service.TestManagerService;
 
 @Service
 public class TestManagerServiceImpl implements TestManagerService {
 	
 	
-	@Autowired
-	TestManagerRepository testManagerRepository;
-
 	@Override
 	public RunExtnDto createRun(TestManagerDto testManagerDto) {
 		RunExtnDto runExtnDto=new RunExtnDto();
@@ -31,7 +27,7 @@ public class TestManagerServiceImpl implements TestManagerService {
 			testManager.tests = testManagerDto.tests;
 			testManager.process = testManagerDto.process;
 			
-			testManager=testManagerRepository.save(testManager);
+			//testManager=testManagerRepository.save(testManager);
 		}
 		runExtnDto.runId = testManager.runId;
 		return runExtnDto;
