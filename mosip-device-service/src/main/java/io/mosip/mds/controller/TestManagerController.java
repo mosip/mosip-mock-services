26f3bcb757b50aa71028f08833a0205d41fd5bbf
Aftab116 <aftabahmed116@gmail.com>
2020-05-18 00:26:56 +0530

    Added Cross Origin(*) to controllers.

--- mosip-device-service/src/main/java/io/mosip/mds/controller/TestManagerController.java
+++ mosip-device-service/src/main/java/io/mosip/mds/controller/TestManagerController.java
@@ -5,12 +5,7 @@ import io.mosip.mds.entitiy.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.mosip.kernel.core.http.ResponseWrapper;
 import io.mosip.mds.dto.TestManagerDto;
@@ -24,7 +19,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-
+@CrossOrigin("*")
 @RestController
 @Api(tags = { "TestManager" })
 @RequestMapping("/testmanager")
