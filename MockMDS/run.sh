5005465a8e4220f30deeb283e75a28947a945f43
ase-101 <>
2020-06-26 15:40:32 +0530

    added run.sh

---
+++ MockMDS/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Starting MOCK MDS service..."
+
+nohup java -cp provider-0.0.1-SNAPSHOT.jar:lib/* org.biometric.provider.ProviderApplication > /tmp/mock_mds.log 2>&1 &
+
+echo "Started MOCK MDS service."
