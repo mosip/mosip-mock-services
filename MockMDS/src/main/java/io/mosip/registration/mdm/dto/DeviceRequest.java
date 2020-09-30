e3acec78ee0fe366ce86c9c15d9671e4868693f5
U156129 <>
2020-09-30 22:44:20 +0530

    data correction

--- MockMDS/src/main/java/io/mosip/registration/mdm/dto/DeviceRequest.java
+++ MockMDS/src/main/java/io/mosip/registration/mdm/dto/DeviceRequest.java
@@ -14,29 +14,9 @@ public class DeviceRequest {
 	public String deviceCode;
 	public String purpose;
 	public String serviceVersion;
-	public String DeviceServiceId;
-	public String DeviceInfoSignature;
-	public String DeviceType;
-	public String DeviceTypeName;
-	public String DeviceProcessName;
-	public String DeviceSubType;
-	public String DeviceProviderName;
-	public String status;
 	public String deviceStatus;
 	public String firmware;
 	public String certification;
-	public String VendorId;
-	public String ProductId;
 	public int[] deviceSubId;
 	public String callbackId;
-    public String Make;
-    public String Model;
-    public String SerialNo;
-    public String DeviceExpiryDate;
-    public String DeviceTimestamp;
-    public String ComplianceLevel;
-    public String HostId;
-    public String ErrorId;
-    public String Otp;
-
 }
