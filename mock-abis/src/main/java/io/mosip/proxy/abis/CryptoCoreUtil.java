d5ea6fd313051d813ee4a54edad9a776dbd978aa
Monobikash Das <M1045447@mindtree.com>
2020-12-18 17:02:38 +0530

    Added prepend thumbprint changes

--- mock-abis/src/main/java/io/mosip/proxy/abis/CryptoCoreUtil.java
+++ mock-abis/src/main/java/io/mosip/proxy/abis/CryptoCoreUtil.java
@@ -2,10 +2,6 @@ package io.mosip.proxy.abis;
 
 import static java.util.Arrays.copyOfRange;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigInteger;
@@ -19,6 +15,8 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.UnrecoverableEntryException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.MGF1ParameterSpec;
@@ -35,31 +33,29 @@ import javax.crypto.spec.OAEPParameterSpec;
 import javax.crypto.spec.PSource.PSpecified;
 import javax.crypto.spec.SecretKeySpec;
 
+import io.mosip.kernel.core.cbeffutil.exception.CbeffException;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.bouncycastle.crypto.InvalidCipherTextException;
 import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.crypto.encodings.OAEPEncoding;
 import org.bouncycastle.crypto.engines.RSAEngine;
 import org.bouncycastle.crypto.params.RSAKeyParameters;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-
-import javassist.bytecode.stackmap.TypeData.ClassName;
 
 @Component
 public class CryptoCoreUtil {
 
-    private final static String RSA_ECB_OAEP_PADDING = "RSA/ECB/OAEPWITHSHA-256ANDMGF1PADDING";
- 
-    
-    private static String certiPassword;
-  
-    private static String alias;
-   
-    private static String keystore;
-  
-    private static String filePath;
+	private final static String RSA_ECB_OAEP_PADDING = "RSA/ECB/OAEPWITHSHA-256ANDMGF1PADDING";
+
+	private static String certiPassword;
+
+	private static String alias;
+
+	private static String keystore;
+
+	private static String filePath;
+
+	private final static int THUMBPRINT_LENGTH = 32;
 
 	public static void setPropertyValues() {
 		Properties prop = new Properties();
@@ -76,17 +72,17 @@ public class CryptoCoreUtil {
 		}
 
 	}
-    
+
 	public String decrypt(String data) throws Exception {
-		PrivateKey privateKey = loadP12();
+		PrivateKeyEntry privateKey = loadP12();
 		byte[] dataBytes = org.apache.commons.codec.binary.Base64.decodeBase64(data);
 		byte[] data1 = decryptData(dataBytes, privateKey);
 		String strData = new String(data1);
 		return strData;
 	}
-	
+
 	public static void setCertificateValues(String filePathVal, String keystoreVal, String passwordVal,
-			String aliasVal) {
+											String aliasVal) {
 		alias = aliasVal;
 		filePath = filePathVal;
 		keystore = keystoreVal;
@@ -94,7 +90,7 @@ public class CryptoCoreUtil {
 
 	}
 
-	public static PrivateKey loadP12() throws KeyStoreException, NoSuchAlgorithmException, CertificateException,
+	public static PrivateKeyEntry loadP12() throws KeyStoreException, NoSuchAlgorithmException, CertificateException,
 			IOException, UnrecoverableEntryException {
 		if (null == certiPassword || certiPassword.isEmpty()) {
 			setPropertyValues();
@@ -104,25 +100,33 @@ public class CryptoCoreUtil {
 		mosipKeyStore.load(fis, certiPassword.toCharArray());
 		ProtectionParameter password = new PasswordProtection(certiPassword.toCharArray());
 		PrivateKeyEntry privateKeyEntry = (PrivateKeyEntry) mosipKeyStore.getEntry(alias, password);
-		PrivateKey privateKey = privateKeyEntry.getPrivateKey();
 
-		return privateKey;
+		return privateKeyEntry;
 	}
 
-	public static byte[] decryptData(byte[] key, PrivateKey privateKey) {
+	public byte[] decryptData(byte[] key, PrivateKeyEntry privateKey) {
 
-        String keySplitter = "#KEY_SPLITTER#";
-        int keyDemiliterIndex = 0;
+		String keySplitter = "#KEY_SPLITTER#";
+		int keyDemiliterIndex = 0;
 		SecretKey symmetricKey = null;
 		byte[] encryptedData = null;
-        final int cipherKeyandDataLength = key.length;
-        final int keySplitterLength = keySplitter.length();
-        keyDemiliterIndex = getSplitterIndex(key, keyDemiliterIndex, keySplitter);
-        byte[] encryptedKey = copyOfRange(key, 0, keyDemiliterIndex);
+		byte[] encryptedSymmetricKey = null;
+		final int cipherKeyandDataLength = key.length;
+		final int keySplitterLength = keySplitter.length();
+		keyDemiliterIndex = getSplitterIndex(key, keyDemiliterIndex, keySplitter);
+		byte[] encryptedKey = copyOfRange(key, 0, keyDemiliterIndex);
 		try {
 			encryptedData = copyOfRange(key, keyDemiliterIndex + keySplitterLength, cipherKeyandDataLength);
-        byte[] decryptedSymmetricKey = asymmetricDecrypt(privateKey, ((RSAPrivateKey) privateKey).getModulus(),
-                encryptedKey);
+			byte[] dataThumbprint = Arrays.copyOfRange(encryptedKey, 0, THUMBPRINT_LENGTH);
+			encryptedSymmetricKey = Arrays.copyOfRange(encryptedKey, THUMBPRINT_LENGTH, encryptedKey.length);
+			byte[] certThumbprint = getCertificateThumbprint(privateKey.getCertificate());
+			/*if (!Arrays.equals(dataThumbprint, certThumbprint)) {
+				throw new CbeffException("Error in generating Certificate Thumbprint.");
+			}*/
+
+			byte[] decryptedSymmetricKey = asymmetricDecrypt(privateKey.getPrivateKey(),
+					((RSAPrivateKey) privateKey.getPrivateKey()).getModulus(),
+					encryptedSymmetricKey);
 			symmetricKey = new SecretKeySpec(decryptedSymmetricKey, 0, decryptedSymmetricKey.length, "AES");
 
 		} catch (Exception e) {
@@ -130,26 +134,26 @@ public class CryptoCoreUtil {
 			e.printStackTrace();
 		}
 		return symmetricDecrypt(symmetricKey, encryptedData, null);
-    }
+	}
 
-    private static int getSplitterIndex(byte[] encryptedData, int keyDemiliterIndex, String keySplitter) {
-        final byte keySplitterFirstByte = keySplitter.getBytes()[0];
-        final int keySplitterLength = keySplitter.length();
-        for (byte data : encryptedData) {
-            if (data == keySplitterFirstByte) {
-                final String keySplit = new String(
-                        copyOfRange(encryptedData, keyDemiliterIndex, keyDemiliterIndex + keySplitterLength));
-                if (keySplitter.equals(keySplit)) {
-                    break;
-                }
-            }
-            keyDemiliterIndex++;
-        }
-        return keyDemiliterIndex;
-    }
+	private static int getSplitterIndex(byte[] encryptedData, int keyDemiliterIndex, String keySplitter) {
+		final byte keySplitterFirstByte = keySplitter.getBytes()[0];
+		final int keySplitterLength = keySplitter.length();
+		for (byte data : encryptedData) {
+			if (data == keySplitterFirstByte) {
+				final String keySplit = new String(
+						copyOfRange(encryptedData, keyDemiliterIndex, keyDemiliterIndex + keySplitterLength));
+				if (keySplitter.equals(keySplit)) {
+					break;
+				}
+			}
+			keyDemiliterIndex++;
+		}
+		return keyDemiliterIndex;
+	}
 
 	/**
-	 * 
+	 *
 	 * @param privateKey
 	 * @param keyModulus
 	 * @param data
@@ -165,28 +169,27 @@ public class CryptoCoreUtil {
 			throws IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException,
 			InvalidAlgorithmParameterException, InvalidKeyException {
 
-        Cipher cipher;
-        try {
-            cipher = Cipher.getInstance(RSA_ECB_OAEP_PADDING);
-            OAEPParameterSpec oaepParams = new OAEPParameterSpec("SHA-256", "MGF1", MGF1ParameterSpec.SHA256,
-                    PSpecified.DEFAULT);
-            cipher.init(Cipher.DECRYPT_MODE, privateKey, oaepParams);
-            return cipher.doFinal(data);
-        } catch (java.security.NoSuchAlgorithmException e) {
-            throw new NoSuchAlgorithmException(e);
-        } catch (NoSuchPaddingException e) {
-            throw new NoSuchPaddingException(e.getMessage());
-        } catch (java.security.InvalidKeyException e) {
-            throw new InvalidKeyException(e);
-        } catch (InvalidAlgorithmParameterException e) {
-            throw new InvalidAlgorithmParameterException(e);
-        }
+		Cipher cipher;
+		try {
+			cipher = Cipher.getInstance(RSA_ECB_OAEP_PADDING);
+			OAEPParameterSpec oaepParams = new OAEPParameterSpec("SHA-256", "MGF1", MGF1ParameterSpec.SHA256,
+					PSpecified.DEFAULT);
+			cipher.init(Cipher.DECRYPT_MODE, privateKey, oaepParams);
+			return cipher.doFinal(data);
+		} catch (java.security.NoSuchAlgorithmException e) {
+			throw new NoSuchAlgorithmException(e);
+		} catch (NoSuchPaddingException e) {
+			throw new NoSuchPaddingException(e.getMessage());
+		} catch (java.security.InvalidKeyException e) {
+			throw new InvalidKeyException(e);
+		} catch (InvalidAlgorithmParameterException e) {
+			throw new InvalidAlgorithmParameterException(e);
+		}
 	}
 
 	/**
 	 *
 	 * @param paddedPlainText
-	 * @param privateKey
 	 * @return
 	 * @throws InvalidCipherTextException
 	 * @throws InvalidKeyException
@@ -194,11 +197,11 @@ public class CryptoCoreUtil {
 	private static byte[] unpadOAEPPadding(byte[] paddedPlainText, BigInteger keyModulus)
 			throws InvalidCipherTextException {
 
-			OAEPEncoding encode = new OAEPEncoding(new RSAEngine(), new SHA256Digest());
-			BigInteger exponent = new BigInteger("1");
-			RSAKeyParameters keyParams = new RSAKeyParameters(false, keyModulus, exponent);
-			encode.init(false, keyParams);
-			return encode.processBlock(paddedPlainText, 0, paddedPlainText.length);
+		OAEPEncoding encode = new OAEPEncoding(new RSAEngine(), new SHA256Digest());
+		BigInteger exponent = new BigInteger("1");
+		RSAKeyParameters keyParams = new RSAKeyParameters(false, keyModulus, exponent);
+		encode.init(false, keyParams);
+		return encode.processBlock(paddedPlainText, 0, paddedPlainText.length);
 	}
 
 	private static byte[] symmetricDecrypt(SecretKey key, byte[] data, byte[] aad) {
@@ -216,8 +219,15 @@ public class CryptoCoreUtil {
 			output = cipher.doFinal(Arrays.copyOf(data, data.length - cipher.getBlockSize()));
 		} catch (Exception e) {
 
-	    }
+		}
 		return output;
-	} 
-}
+	}
 
+	public byte[] getCertificateThumbprint(Certificate cert) throws CertificateEncodingException {
+		try {
+			return DigestUtils.sha256(cert.getEncoded());
+		} catch (CertificateEncodingException e) {
+			throw e;
+		}
+	}
+}
