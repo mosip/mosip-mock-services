86c4af43f33238c0c70a1ab427f4ef8e971e94f6
ankitvaishnav <ankitvaishnav.iitb1991@gmail.com>
2021-06-30 23:16:09 +0530

    Minor fixes

--- mock-abis/src/main/java/io/mosip/proxy/abis/configuration/SwaggerConfig.java
+++ mock-abis/src/main/java/io/mosip/proxy/abis/configuration/SwaggerConfig.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -15,8 +19,16 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build()
+				.apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		return new ApiInfo("Mock ABIS", "Mock ABIS", "1.0", "Terms of service",
+				new Contact("MOSIP Mock ABIS", "https://mosip.io", "info@mosip.io"), "Apache License Version 2.0",
+				"https://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
 	}
 
 }
