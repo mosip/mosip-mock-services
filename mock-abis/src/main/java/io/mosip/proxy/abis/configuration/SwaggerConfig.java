fb529514ab1be23a1d09b096c828c791b44df696
kameshsr <kameshsr1338@gmail.com>
2021-12-02 12:01:12 +0530

    MOSIP-18385 Upgraded swagger2 to openapi3 for mock-abis service.

--- mock-abis/src/main/java/io/mosip/proxy/abis/configuration/SwaggerConfig.java
+++ mock-abis/src/main/java/io/mosip/proxy/abis/configuration/SwaggerConfig.java
@@ -1,34 +1,49 @@
 package io.mosip.proxy.abis.configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
-
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
+	private static final Logger logger = LoggerFactory.getLogger(SwaggerConfig.class);
+
+	@Autowired
+	private OpenApiProperties openApiProperties;
+
 	@Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build()
-				.apiInfo(metaData());
+	public OpenAPI openApi() {
+		OpenAPI api = new OpenAPI()
+				.components(new Components())
+				.info(new Info()
+						.title(openApiProperties.getInfo().getTitle())
+						.version(openApiProperties.getInfo().getVersion())
+						.description(openApiProperties.getInfo().getDescription())
+						.license(new License()
+								.name(openApiProperties.getInfo().getLicense().getName())
+								.url(openApiProperties.getInfo().getLicense().getUrl())));
+
+		openApiProperties.getService().getServers().forEach(server -> {
+			api.addServersItem(new Server().description(server.getDescription()).url(server.getUrl()));
+		});
+		logger.info("swagger open api bean is ready");
+		return api;
 	}
 
-	private ApiInfo metaData() {
-		return new ApiInfo("Mock ABIS", "Mock ABIS", "1.0", "Terms of service",
-				new Contact("MOSIP Mock ABIS", "https://mosip.io", "info@mosip.io"), "Apache License Version 2.0",
-				"https://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
+	@Bean
+	public GroupedOpenApi groupedOpenApi() {
+		return GroupedOpenApi.builder().group(openApiProperties.getGroup().getName())
+				.pathsToMatch(openApiProperties.getGroup().getPaths().stream().toArray(String[]::new))
+				.build();
 	}
 
 }
