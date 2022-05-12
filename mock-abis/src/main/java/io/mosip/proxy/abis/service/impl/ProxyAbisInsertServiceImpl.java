f69e0f29463e6f00b9616ba9f85553a7e90add30
Monobikash Das <monobikash.das@mindtree.com>
2022-05-12 18:36:13 +0530

    added force duplicate

--- mock-abis/src/main/java/io/mosip/proxy/abis/service/impl/ProxyAbisInsertServiceImpl.java
+++ mock-abis/src/main/java/io/mosip/proxy/abis/service/impl/ProxyAbisInsertServiceImpl.java
@@ -274,7 +274,8 @@ public class ProxyAbisInsertServiceImpl implements ProxyAbisInsertService {
 			String refId = ir.getReferenceId();
             logger.info("Checking for duplication of reference ID " + refId);
 			List<BiometricData> lst = null;
-			logger.info("return duplicate property set to "+proxyAbisConfigService.getDuplicate());
+			logger.info("find duplicate property set to " + proxyAbisConfigService.getDuplicate());
+			logger.info("force duplicate property set to " + proxyAbisConfigService.isForceDuplicate());
 			if (null != ir.getGallery() && ir.getGallery().getReferenceIds().size() > 0
 					&& null != ir.getGallery().getReferenceIds().get(0).getReferenceId()
 					&& !ir.getGallery().getReferenceIds().get(0).getReferenceId().isEmpty()) {
