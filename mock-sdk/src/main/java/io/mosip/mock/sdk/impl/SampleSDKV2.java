73ddcebb684d079cfe050d355f70dca94dd471ad
JanardhanBS-SyncByte <janardhan@syncbyte.in>
2023-04-03 17:31:01 +0530

    DSD-2242

--- mock-sdk/src/main/java/io/mosip/mock/sdk/impl/SampleSDKV2.java
+++ mock-sdk/src/main/java/io/mosip/mock/sdk/impl/SampleSDKV2.java
@@ -14,26 +14,25 @@ import io.mosip.kernel.biometrics.model.MatchDecision;
 import io.mosip.kernel.biometrics.model.QualityCheck;
 import io.mosip.kernel.biometrics.model.Response;
 import io.mosip.kernel.biometrics.model.SDKInfo;
-import io.mosip.kernel.biometrics.spi.IBioApi;
+import io.mosip.kernel.biometrics.spi.IBioApiV2;
 import io.mosip.mock.sdk.service.CheckQualityService;
+import io.mosip.mock.sdk.service.ConvertFormatService;
 import io.mosip.mock.sdk.service.ExtractTemplateService;
 import io.mosip.mock.sdk.service.MatchService;
 import io.mosip.mock.sdk.service.SDKInfoService;
 import io.mosip.mock.sdk.service.SegmentService;
 
 /**
- * The Class BioApiImpl.
+ * The Class IBioApiV2Impl.
  * 
- * @author Sanjay Murali
- * @author Manoj SP
+ * @author Janardhan B S
  * 
  */
 @Component
 @EnableAutoConfiguration
-@Deprecated
-public class SampleSDK implements IBioApi {
+public class SampleSDKV2 implements IBioApiV2 {
 
-	Logger LOGGER = LoggerFactory.getLogger(SampleSDK.class);
+	Logger LOGGER = LoggerFactory.getLogger(SampleSDKV2.class);
 
 	private static final String API_VERSION = "0.9";
 
@@ -66,6 +65,14 @@ public class SampleSDK implements IBioApi {
 	}
 
 	@Override
+	public Response<BiometricRecord> convertFormatV2(BiometricRecord record, String sourceFormat, String targetFormat,
+			Map<String, String> sourceParams, Map<String, String> targetParams,
+			List<BiometricType> modalitiesToConvert) {
+		ConvertFormatService service = new ConvertFormatService(record, sourceFormat, targetFormat, sourceParams, targetParams, modalitiesToConvert);
+		return service.getConvertFormatInfo();
+	}
+
+	@Override
 	public Response<BiometricRecord> segment(BiometricRecord sample, List<BiometricType> modalitiesToSegment,
 			Map<String, String> flags) {
 		SegmentService service = new SegmentService(sample, modalitiesToSegment, flags);
@@ -73,6 +80,7 @@ public class SampleSDK implements IBioApi {
 	}
 
 	@Override
+	@Deprecated
 	public BiometricRecord convertFormat(BiometricRecord sample, String sourceFormat, String targetFormat,
 			Map<String, String> sourceParams, Map<String, String> targetParams,
 			List<BiometricType> modalitiesToConvert) {
