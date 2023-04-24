8263bb4f1ade14b787c6bd8daa3786ea4eebaf95
Neeharika Garg <Neeharika.Garg@technoforte.co.in>
2023-04-24 17:46:34 +0530

    added import exception
    Signed-off-by: JanardhanBS-SyncByte <janardhan@syncbyte.in>

--- MockMDS/src/main/java/io/mosip/mock/sbi/service/SBIMockService.java
+++ MockMDS/src/main/java/io/mosip/mock/sbi/service/SBIMockService.java
@@ -5,6 +5,7 @@ import java.net.UnknownHostException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 
 import io.mosip.mock.sbi.util.FileHelper;
