028a62a340d8176ae8ba64c735bff0326c535383
JanardhanBS-SyncByte <72377118+JanardhanBS-SyncByte@users.noreply.github.com>
2024-03-26 11:42:36 +0530

    Update SampleSDK.java
    Review request minimum changes 
    
    Signed-off-by: JanardhanBS-SyncByte <72377118+JanardhanBS-SyncByte@users.noreply.github.com>

--- mock-sdk/src/main/java/io/mosip/mock/sdk/impl/SampleSDK.java
+++ mock-sdk/src/main/java/io/mosip/mock/sdk/impl/SampleSDK.java
@@ -44,6 +44,7 @@ public class SampleSDK implements IBioApi {
 
 	@Override
 	public SDKInfo init(Map<String, String> initParams) {
+		// TODO validate for mandatory initParams
 		SDKInfoService service = new SDKInfoService(env, API_VERSION, "sample", "sample", "sample");
 		return service.getSDKInfo();
 	}
