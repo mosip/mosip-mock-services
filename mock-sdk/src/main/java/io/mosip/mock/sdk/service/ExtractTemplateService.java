855ec74a0f7e0c5b1a14a736956958faf726581e
JanardhanBS-SyncByte <72377118+JanardhanBS-SyncByte@users.noreply.github.com>
2024-03-26 12:52:32 +0530

    Update ExtractTemplateService.java
    Signed-off-by: JanardhanBS-SyncByte <72377118+JanardhanBS-SyncByte@users.noreply.github.com>

--- mock-sdk/src/main/java/io/mosip/mock/sdk/service/ExtractTemplateService.java
+++ mock-sdk/src/main/java/io/mosip/mock/sdk/service/ExtractTemplateService.java
@@ -46,7 +46,7 @@ public class ExtractTemplateService extends SDKService {
 			}
 
 			if (sample != null)
-				LOGGER.error("extractTemplate -- error", sample.getSegments().size());
+				LOGGER.info("extractTemplate {}", sample.getSegments().size());
 				
 			for (BIR segment : sample.getSegments()) {
 				if (!isValidBirData(segment))
